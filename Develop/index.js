fdd305d620913eb9daabb2194ef95a554a66883f
Jasmine Ulloa <ulloajazz6@gmail.com>
2023-01-23 19:42:31 -0800

    changed prompt format and added pseudocode for functions

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,7 @@
 const { default: inquirer } = require("inquirer");
 
 //Include packages needed for this application
-const inquirer = require('inquirer');
+const inquirer = require("inquirer");
 const fs = require("fs");
 const generateMarkdown = require("./utils/generateMarkdown");
 
@@ -9,64 +9,72 @@ const generateMarkdown = require("./utils/generateMarkdown");
 const questions = [
   {
     type: "input",
-    message: "What is your First and Last name?",
+    message: "What is your name?",
     answer: "name",
   },
-    {
-      type: "input",
-      message: "What is the title of your Project?",
-      answer: "title",
-    },
-    {
-      type: "options",
-      message: "Choose a License",
-      choices:["Apache License 2.0", "General Public License v3.0", "MIT License"]
-    },
-    {
-      type: "input",
-      message: "Table of Contents",
-      answer: "confirm",
-    },
-    {
-      type: "input",
-      message: "Usage",
-      answer: "usage",
-    },
-    {
-      type: "input",
-      message: "Write a description",
-      answer: "description",
-    },
-    {
-      type: "input",
-      message: "List any contributors",
-      answer: "contributors",
-    },
-    {
-      type: "input",
-      message: "Tests",
-      answer: "test",
-    },
-    {
-      type: "input",
-      message: "Questions",
-      answer: "questions",
-    },
-    {message: "Github User name",
-    answer: "username"},  
-    {message: "Enter your email address",
-    answer: "email"}, 
-  ];
+  {
+    type: "input",
+    message: "What is the title of your Project?",
+    answer: "title",
+  },
+  {
+    type: "options",
+    message: "Choose a License",
+    choices: ["Apache", "General Public License", "MIT"],
+  },
+  {
+    type: "input",
+    message: "Table of Contents",
+    answer: "confirm",
+  },
+  {
+    type: "input",
+    message: "Usage",
+    answer: "usage",
+  },
+  {
+    type: "input",
+    message: "Write a description of your project",
+    answer: "description",
+  },
+  {
+    type: "input",
+    message: "List any contributors",
+    answer: "contributors",
+  },
+  {
+    type: "input",
+    message: "Tests",
+    answer: "test",
+  },
+  {
+    type: "input",
+    message: "Questions",
+    answer: "questions",
+  },
+  { message: "What is your GitHub username?", answer: "username" },
+  { message: "Enter your email address", answer: "email" },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
-    //take their responses and generate read me
+  var file = generateMarkdown(data);
+  fs.writeFile(fileName, content, function (error) {
+    if (error) {
+      return console.log(error);
+    }
+    console.log("success");
+  });
+  //take their responses and generate read me
 }
 
-// TODO: Create a function to initialize app
+//Create a function to initialize app
 function init() {
-    //prompt the user with the questions
+  inquirer.prompt(questions).then(function (data) {
+    var fileName = "README.md";
 
+    writeToFile(fileName, data);
+  });
 }
 
 // Function call to initialize app
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,14 +1,40 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
+// https://gist.github.com/lukas-h/2a5d00690736b4c3a7ba
+
+
+const fs = require('fs');
+//Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if(license){
+    return `[![${license} license](https://img.shields.io/badge/License-${license}-blue.svg)](${renderLicenseLink(license)})`
+  }else{
+    return ``;
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+if (license === 'Apache'){
+  return `link`
+}
+if (license === 'MIT'){
+  return `link`
+}
+if (license === 'General Public License')
+ return ``
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (!license){
+    return ``
+  }else{
+    return `## Licenses 
+    ${license}`
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
